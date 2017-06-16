8d0451735f435f29a220c6e70cf0effea6b8af9e
Martin Halamíček <martin@halamicek.cz>
2017-06-16 09:05:46 +0200

    deploy to dev portal ECR

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/db-writer-redshift quay.io/keboola/db-writer-redshift:$TRAVIS_TAG
-docker push quay.io/keboola/db-writer-redshift:$TRAVIS_TAG
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.wr-redshift-v2`
+docker tag keboola/db-writer-redshift:latest $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/db-writer-redshift:latest $REPOSITORY:latest
+eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.wr-redshift-v2)
+docker push $REPOSITORY:$TRAVIS_TAG
+docker push $REPOSITORY:latest
